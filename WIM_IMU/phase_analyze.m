6076640e4306c49df6a2b96a00a871b546d903b3
JW in WIR <jwlee2218@snu.ac.kr>
2023-03-07 16:48:19 +0900

    double support + swing 을 기반으로 하는 phase 분석

--- WIM_IMU/phase_analyze.m
+++ WIM_IMU/phase_analyze.m
@@ -1,23 +1,54 @@
-function [mean_stance, mean_swing] = phase_analyze(time, acc, hs_index, to_index)
+function [mean_stance_L, mean_stance_R, mean_swing_L, mean_swing_R] = phase_analyze(time, hs_index, to_index, stance, thld_DS, thld_SW)
+if nargin < 6
+    thld_SW = 0.5;
+end
+if nargin < 5
+    thld_DS = 0.5;
+end
+% Stance Left = double support(Left) + Swing(Right) + double support(Right)
+% Stance Right = double support(Right) + Swing(Left) + double support(Left)
 
-test_time_20 = time;
-test_fa_20 = acc;
+phase = [hs_index, to_index];
+DS_left = []; % double support
+DS_right = [];
+Swing_left = [];
+Swing_right = [];
 
-hs_index_2rows = reshape(hs_index(1:2*floor(length(hs_index)/2)), 2, [])';
-to_index_2rows = reshape(to_index(1:2*floor(length(to_index)/2)), 2, [])';
+for i = 1:length(stance)
+    DS = time(phase(i,2)) - time(phase(i,1));
+    if DS < thld_DS
+        if stance(i) == 1
+            DS_left = [DS_left; DS];
+        else
+            DS_right = [DS_right; DS];
+        end
+    end
+    if i > 1
+        SW = time(phase(i,1)) - time(phase(i-1,2));
+        if SW < thld_SW
+            if stance(i) == 1
+                Swing_left = [Swing_left; SW];
+            else
+                Swing_right = [Swing_right; SW];
+            end
+        end
+    end 
+end
+mean_stance_L = mean(DS_left) + mean(Swing_right) + mean(DS_right);
+mean_swing_L = mean(Swing_left);
+proportion_stanceL = mean_stance_L / (mean_stance_L + mean_swing_L);
+proportion_swingL = mean_swing_L / (mean_stance_L + mean_swing_L);
 
-hs_time = test_time_20(hs_index_2rows);
-to_time = test_time_20(to_index_2rows);
-stride_time = mean(diff(hs_time, 1, 1), 1);
-swing = [hs_time(2:end,1) - to_time(1:end-1, 2), hs_time(2:end, 2) - to_time(2:end , 1)];
-stance = [to_time(1:end-1, 2) - hs_time(1:end-1, 1), to_time(2:end, 1) - hs_time(1:end-1, 2)];
+mean_stance_R = mean(DS_right) + mean(Swing_left) + mean(DS_left);
+mean_swing_R = mean(Swing_right);
+proportion_stanceR = mean_stance_R / (mean_stance_R + mean_swing_R);
+proportion_swingR = mean_swing_R / (mean_stance_R + mean_swing_R);
 
-mean_stance = mean(stance, 1);
-std_stance = std(stance, 1);
-mean_swing = mean(swing, 1);
-std_swing = std(swing, 1);
+std_swing_L = std(Swing_left) / (mean_stance_L + mean_swing_L);
+std_swing_R = std(Swing_right) / (mean_stance_R + mean_swing_R);
 
-gait_phase = [mean_stance, mean_swing];
+
+gait_phase = [proportion_stanceL, proportion_stanceR, proportion_swingL, proportion_swingR];
 b = barh(gait_phase, 'facecolor', 'flat');
 b.CData = [
 0, 0.4470, 0.7410;
@@ -26,7 +57,7 @@ b.CData = [
 0.9290, 0.6940, 0.1250];
 yticklabels({'Stance Left', 'Stance Right', 'Swing Left', 'Swing Right'})
 hold on
-err = [std_stance, std_swing];
+err = [std_swing_L, std_swing_R, std_swing_L, std_swing_R];
 er = errorbar(gait_phase, 1:4,  err, 'horizontal');
 er.Color = [0 0 0];
 er.LineStyle = 'none';
